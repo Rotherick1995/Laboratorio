03bb887358f97b59be327f2b832961756aa55f49
Rotherick <rotherickcalderon.git@gmail.com>
2025-03-01 20:17:05 -0400

    proj

--- proyecto2.sql
+++ proyecto2.sql
@@ -2,7 +2,7 @@ create database laboratorio;
 use laboratorio;
 
 create table tpacientes (
-    idpacientes int primary key auto_increment,
+    idpacientes int primary key ,
     nombre varchar(50) not null,
     apellidopaterno varchar(50) not null,
     apellidomaterno varchar(50) not null, 
@@ -15,7 +15,7 @@ create table tpacientes (
 );
 
 create table tmedicos (
-    idmedicos int primary key auto_increment,
+    idmedicos int primary key,
     nombre varchar(50) not null,
     apellidopaterno varchar(50) not null,
     apellidomaterno varchar(50) not null,
@@ -24,16 +24,16 @@ create table tmedicos (
 );
 
 create table tsolicitud (
-    idsolicitud int primary key auto_increment,
+    idsolicitud int primary key,
     fechasolicitud datetime not null,
     idpacientes int not null,
     idmedicos int not null,
-    foreign key (idpacientes) references tpacientes(idpacientes) on delete cascade,
-    foreign key (idmedicos) references tmedicos(idmedicos) on delete cascade
+    foreign key (idpacientes) references tpacientes(idpacientes),
+    foreign key (idmedicos) references tmedicos(idmedicos) 
 );
 
 create table treporte (
-    idreporte int primary key auto_increment,
+    idreporte int primary key,
     fechainicio date not null,
     fechaentrega date not null,
     prioridad varchar(20),
@@ -44,32 +44,32 @@ create table treporte (
 );
 
 create table tarealaboratorio (
-    idarea int primary key auto_increment,
+    idarea int primary key ,
     nombre varchar(50) not null,
     descripcion varchar(100)
 );
 
 create table tprueba (
-    idprueba int primary key auto_increment,
+    idprueba int primary key ,
     nombre varchar(50) not null,
     descripcion varchar(100),
     valorreferencia double not null,
     idarea int not null,
-    foreign key (idarea) references tarealaboratorio(idarea) on delete cascade
+    foreign key (idarea) references tarealaboratorio(idarea)
 );
 
 create table tdetallesolicitud (
-    iddetallesol int primary key auto_increment,
+    iddetallesol int primary key,
     idsolicitud int not null,
     idprueba int not null,
-    foreign key (idsolicitud) references tsolicitud(idsolicitud) on delete cascade,
-    foreign key (idprueba) references tprueba(idprueba) on delete cascade
+    foreign key (idsolicitud) references tsolicitud(idsolicitud) ,
+    foreign key (idprueba) references tprueba(idprueba) 
 );
 
 create table tresultados (
-    idresultado int primary key auto_increment,
+    idresultado int primary key ,
     iddetallesol int not null,
     resultado double not null,
     unidad varchar(20),
-    foreign key (iddetallesol) references tdetallesolicitud(iddetallesol) on delete cascade
+    foreign key (iddetallesol) references tdetallesolicitud(iddetallesol) 
 );
